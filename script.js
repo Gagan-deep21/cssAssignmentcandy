37a1c8c79c1fc9fdc16b4a154dad3947de64a78d
Gagan-deep21 <136130857+Gagan-deep21@users.noreply.github.com>
2023-07-22 07:54:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var toggleButton = document.getElementById('toggle-social-media');
+  var socialMediaIcons = document.querySelector('.social-media');
+
+  toggleButton.addEventListener('click', function() {
+    socialMediaIcons.classList.toggle('hidden');
+  });
+});
